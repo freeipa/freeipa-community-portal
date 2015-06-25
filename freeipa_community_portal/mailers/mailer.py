9108284b5e72588dc9edfc0212b25299a0c97b2f
Drew Erny <derny@redhat.com>
2015-06-25 13:44:12 -0400

    fixed a bug and added licensing information

--- freeipa_community_portal/mailers/mailer.py
+++ freeipa_community_portal/mailers/mailer.py
@@ -1,13 +1,32 @@
+# Authors:
+#   Drew Erny <derny@redhat.com>
+#
+# Copyright (C) 2015  Red Hat
+# see file 'COPYING' for use and warranty information
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """ Contains the base class for sending mail """
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 # import smtplib
 
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, PackageLoader
 
 class Mailer(object):
     """ Base class for sending mail """
-    env = Environment(loader=FileSystemLoader('mailers/templates'))
+    env = Environment(loader=PackageLoader('freeipa_community_portal.mailers','templates'))
 
     def __init__(self):
         self.subject = "FreeIPA Community Portal: Notice"
--- freeipa_community_portal/model/user.py
+++ freeipa_community_portal/model/user.py
@@ -1,3 +1,22 @@
+# Authors:
+#   Drew Erny <derny@redhat.com>
+#
+# Copyright (C) 2015  Red Hat
+# see file 'COPYING' for use and warranty information
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """ User model """
 from ipalib import api, errors
 
@@ -38,6 +57,9 @@ class User(object): # pylint: disable=too-few-public-methods
     def _call_api(self):
         """performs the actual API call. seperate method for testing purposes
         """
+        if not api.Backend.rpcclient.isconnected():
+            api.Backend.rpcclient.connect()
+
         api.Command.user_add( # pylint: disable=no-member
             givenname=self.given_name,
             sn=self.family_name,
