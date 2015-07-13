f4929a8435fd36d02a8e6fb8db1dbb9d2a83cc5c
Drew Erny <derny@redhat.com>
2015-07-13 12:52:09 -0400

    captcha now cannot be replayed

--- freeipa_community_portal/model/captcha_wrapper.py
+++ freeipa_community_portal/model/captcha_wrapper.py
@@ -1,14 +1,29 @@
 """ Module for handling captchas in the community portal. """
 
 from captcha.image import ImageCaptcha
+from datetime import datetime, timedelta
+
 import string
 import random
 import base64
 import hmac
 
+from sqlalchemy import Table, Column, MetaData, String, DateTime, create_engine
+from sqlalchemy.sql import select, insert, delete
+
 LENGTH = 4
 # TODO: this is not a secure key
 KEY = 'lol you should probably change this'
+
+#TODO: fix this so that it uses in-memory database
+_engine = create_engine('sqlite:///captcha.db', echo=True)
+_metadata = MetaData()
+_captcha = Table('captcha', _metadata,
+    Column('hmac', String, primary_key=True),
+    Column('timestamp', DateTime)
+)
+_metadata.create_all(_engine)
+USE_BY = timedelta(days=1)
 
 class CaptchaHelper(object):
     """Class for making a captcha for the client to display."""
@@ -23,6 +38,15 @@ class CaptchaHelper(object):
 
         # generate the captcha image, hold it as bytes
         self.image = self.image_generator.generate(self.solution, format='jpeg').getvalue()
+        conn = _engine.connect()
+        conn.execute(
+            _captcha.insert().values(
+                hmac=self.solution_hash(),
+                timestamp=datetime.now()
+            )
+        )
+        conn.close()
+            
 
     def datauri(self):
         """Returns the captcha image to a data-uri, in jpeg format"""
@@ -38,6 +62,19 @@ class CaptchaHelper(object):
 
 def checkResponse(response, solution):
     """Compares a given solution hash with the response provided"""
+    valid = False
     digest = hmac.new(KEY, response.upper()).hexdigest()
-    return hmac.compare_digest(digest, solution.encode('ascii','ignore'))
+    if hmac.compare_digest(digest, solution.encode('ascii','ignore')): 
+        conn = _engine.connect()
+        result = conn.execute(
+            select([_captcha]).where(_captcha.c.hmac == digest)
+        )
+        row = result.first()
+        if row is not None:
+            valid = True
+            conn.execute(
+                delete(_captcha).where(_captcha.c.hmac==row['hmac'])
+            )
+        conn.close()
+    return valid
 
